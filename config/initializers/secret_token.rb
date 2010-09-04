d5e465ee287c09d352a833c467bd2d452f44b09d
jose ovando <jose@jose-laptop.(none)>
2010-09-03 22:47:39 -0400

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ayni::Application.config.secret_token = '5a42d0f7c4ec04062c82cbaf0ee2101db43be7e1a1f99667cdeca83c8c1a039686811b577d0e35711fd5ff7c692af38415a13ca04c9d6b8e9ffa59c72635ce63'
